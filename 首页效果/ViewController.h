04527e6b593db1f4f5288337073b01ceb8861860
xiaohuihui508 <15136718290@163.com>
2017-02-10 11:10:52 +0800

    Initial Commit

---
+++ 首页效果/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  首页效果
+//
+//  Created by mac on 17/2/10.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 首页效果/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  首页效果
+//
+//  Created by mac on 17/2/10.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
